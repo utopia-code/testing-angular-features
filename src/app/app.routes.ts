c7b5529de55003826845ba003a8e8d313d7b1b2e
utopia-code <utopiacode.developer@gmail.com>
2024-06-22 15:37:04 +0200

    PEC6: set lazy loading for modules home, list, statistics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadChildren: () => 
+            import('./home/home.module').then((m) => m.HomeModule)
+    },
+    {
+        path: 'list',
+        loadChildren: () => 
+            import('./list/list.module').then((m) => m.ListModule)
+    },
+    {
+        path: 'statistics',
+        loadChildren: () => 
+            import('./statistics/statistics.module').then((m) => m.StatisticsModule)
+    }
+];
