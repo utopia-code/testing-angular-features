9b405c59d7e5e1512c7f817200e282ab5ce3700d
utopia-code <utopiacode.developer@gmail.com>
2024-06-23 12:45:02 +0200

    PEC6: list students with for in home component

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StudentDTO } from '../../Models/student.dto';
+import { StudentService } from '../../Services/student.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  students: StudentDTO[] = [];
+  columnsTable: string[] = ['Nombre', 'Género', 'Nota', 'Ausencias'];
+  
+  constructor(private studentService: StudentService) {}
 
+  ngOnInit(): void {
+    this.studentService.getStudents().subscribe((data) => {
+
+      data.forEach(item => {
+        item.genre = this.getGenre(item.genre)
+      });
+
+      this.students = data;
+      // this.students = [];
+    })
+  }
+
+  getGenre(genre: string): string {
+    switch (genre) {
+      case 'M':
+        return 'Masculino';
+      case 'F': 
+        return 'Femenino';
+      default:
+        return 'No binario';
+    }
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
- 
 
 @NgModule({
   declarations: [HomePageComponent],
