325e4db052d45587112fb5f96b1a106c853df5c7
utopia-code <utopiacode.developer@gmail.com>
2024-06-24 14:05:54 +0200

    PEC6: set part of components in template of statistics page component / module

--- src/app/statistics/statistics-page/statistics-page.component.ts
+++ src/app/statistics/statistics-page/statistics-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
-  styleUrl: './statistics-page.component.css'
+  styleUrl: './statistics-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StatisticsPageComponent {
 
--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { GenderComponent } from './gender/gender.component';
+import { GeneralResultsComponent } from './general-results/general-results.component';
+import { ResumeDataComponent } from './resume-data/resume-data.component';
 import { StatisticsPageComponent } from './statistics-page/statistics-page.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
 
 @NgModule({
-  declarations: [StatisticsPageComponent],
+  declarations: [
+    StatisticsPageComponent, 
+    ResumeDataComponent, 
+    GenderComponent, 
+    GeneralResultsComponent
+  ],
   imports: [
     CommonModule,
     StatisticsRoutingModule
