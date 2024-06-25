0a43b0def2ae485a8007eac586ba265afc8b9012
utopia-code <utopiacode.developer@gmail.com>
2024-06-25 12:31:24 +0200

    PEC6: subscribe students type StudentDTO to studentService, import ChartConfiguration and set value variables data chart in resume-data component / add canvas chart in template type doughnut / add ChangeDetectorRef to load chart

--- src/app/statistics/resume-data/resume-data.component.ts
+++ src/app/statistics/resume-data/resume-data.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { StudentDTO } from '../../Models/student.dto';
+import { StudentService } from '../../Services/student.service';
 
 @Component({
   selector: 'app-resume-data',
   templateUrl: './resume-data.component.html',
-  styleUrl: './resume-data.component.css'
+  styleUrl: './resume-data.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ResumeDataComponent {
+export class ResumeDataComponent implements OnInit {
+  students: StudentDTO[] = [];
 
+  public doughnutChartLabels: string[] = [
+    'Alumnos',
+    'Aprobados',
+    'Suspendidos'
+  ];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+    { data: [], label: '' },
+  ];
+
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'];
+
+  constructor(
+    private studentService: StudentService, 
+    private cdr: ChangeDetectorRef
+  ) {
+    console.log('LoadingSlowComponent');
+
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+
+  }
+
+  ngOnInit(): void {
+    
+    this.studentService.getStudents().subscribe((students) => {
+      this.students = students;
+
+      const totalStudents = this.students.length;
+
+      const { totalPassStudents, totalFailStudents } = this.students.reduce(
+        (acc, student) => {
+          if (student.note >= 5) {
+            acc.totalPassStudents += 1;
+          } else {
+            acc.totalFailStudents += 1;
+          }
+
+          return acc
+        }, { totalPassStudents: 0, totalFailStudents: 0 }
+      )
+
+      this.doughnutChartDatasets = [
+        { data: [totalStudents, totalPassStudents, totalFailStudents], label: 'Total' },
+      ];
+
+      this.cdr.markForCheck();
+    })
+  }
 }
