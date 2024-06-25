41ba33b692decf9409419da9104591f9ec84fc27
utopia-code <utopiacode.developer@gmail.com>
2024-06-25 12:31:51 +0200

    PEC6: subscribe students type StudentDTO to studentService, import ChartConfiguration and set value variables data chart in gender component / add canvas chart in template type pie / add ChangeDetectorRef to load chart

--- src/app/statistics/gender/gender.component.ts
+++ src/app/statistics/gender/gender.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { StudentDTO } from '../../Models/student.dto';
+import { StudentService } from '../../Services/student.service';
 
 @Component({
   selector: 'app-gender',
   templateUrl: './gender.component.html',
   styleUrl: './gender.component.css'
 })
-export class GenderComponent {
+export class GenderComponent implements OnInit {
+  students: StudentDTO[] = [];
 
+  public pieChartLabels: string[] = [
+    'Masculino',
+    'Femenino'
+  ]
+
+  public pieChartDatasets: ChartConfiguration<'pie'>['data']['datasets'] = [
+    { data: [], label: '' }
+  ];
+
+  public pieChartOptions: ChartConfiguration<'pie'>['options'];
+
+  constructor(
+    private studentService: StudentService,
+    private cdr: ChangeDetectorRef
+  ) {
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+  }
+
+  ngOnInit(): void {
+    this.studentService.getStudents().subscribe((data) => {
+      this.students = data;
+    
+      console.log(this.students)
+
+      const { totalMen, totalWomen } = this.students.reduce(
+        (acc, student) => {
+          if (student.genre == 'Masculino') {
+            acc.totalMen += 1;
+          } else {
+            acc.totalWomen += 1;
+          }
+
+          return acc
+        }, { totalMen: 0, totalWomen: 0 }
+      )
+
+      this.pieChartDatasets = [
+        { data: [totalMen, totalWomen], label: 'Total' }
+      ]
+
+      this.cdr.markForCheck();
+    })
+  }
 }
